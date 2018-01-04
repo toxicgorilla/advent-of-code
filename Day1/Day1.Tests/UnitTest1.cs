b99dddf1abb0e0cafbeedc6f3cf777e0b7ed1fe5
Stephen Hughes <steve.hughes@creative-technologies.co.uk>
2018-01-04 11:35:16 +0000

    Got rid of xUnit Should. Updated NuGet packages

--- Day1/Day1.Tests/UnitTest1.cs
+++ Day1/Day1.Tests/UnitTest1.cs
@@ -4,38 +4,18 @@ namespace Day1.Tests
 
     using Xunit;
 
-    using XunitShould;
-
     public class UnitTest1
     {
-        [Fact]
-        public void With_Input_1122_Answer_Is_3()
+        [Theory]
+        [InlineData("1122", 3)]
+        [InlineData("1111", 4)]
+        [InlineData("1234", 0)]
+        // ReSharper disable once InconsistentNaming
+        public void With_Input_X_Answer_Is_Y(string input, int expectedAnswer)
         {
-            var input = "1122";
-            var expected = 3;
-            var actual = ReverseCaptcha.CalculateAnswer(input);
+            var actualAnswer = ReverseCaptcha.CalculateAnswer(input);
 
-            actual.ShouldEqual(expected);
-        }
-
-        [Fact]
-        public void With_Input_1111_Answer_Is_4()
-        {
-            var input = "1111";
-            var expected = 4;
-            var actual = ReverseCaptcha.CalculateAnswer(input);
-
-            actual.ShouldEqual(expected);
-        }
-
-        [Fact]
-        public void With_Input_1234_Answer_Is_0()
-        {
-            var input = "1234";
-            var expected = 0;
-            var actual = ReverseCaptcha.CalculateAnswer(input);
-
-            actual.ShouldEqual(expected);
+            Assert.Equal(actualAnswer, expectedAnswer);
         }
     }
 }
